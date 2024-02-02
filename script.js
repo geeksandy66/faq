c539db07fd6c059ec8292abcd563910d709e5c3a
geeksandy66 <geeksandy66@gmail.com>
2024-02-02 08:22:49 +0530

    js clean code

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
 const faqs = document.querySelectorAll('.question-answer');
-// let icon = document.querySelectorAll(".icon");
-
-// for(let i=0; i<faqs.length; i++){
-//     faqs[i].addEventListener('click',()=>{
-//         faqs[i].classList.toggle('active');
-//         icon[i].classList.toggle('minus');
-//     })
-// }
 
 faqs.forEach((faq, index)=>{
     faq.addEventListener('click', ()=>{
